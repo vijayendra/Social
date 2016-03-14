cb237af1562e04a567e5436827c96afd574668fe
Vijayendra Bapte <vbapte@egnyte.com>
2016-03-14 16:36:51 -0700

    more redability

--- forum/models.py
+++ forum/models.py
@@ -8,10 +8,17 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
+    def __unicode__(self):
+        return "<Post(%s): %s>" % (self.pk, self.title)
+
 
 class Comment(models.Model):
     user = models.ForeignKey(User, related_name='user_comments')
     post = models.ForeignKey('Post', related_name='comments')
     parent = models.ForeignKey('self', related_name='childs', null=True, default=None)
     description = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return "<Comment(%s): %s>" % (self.pk, self.description)
+
     
