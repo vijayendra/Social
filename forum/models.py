7e00869299f694def52a57256a7cf4c6f01efa82
Vijayendra Bapte <vbapte@egnyte.com>
2016-03-15 10:59:06 -0700

    one user can have many posts

--- forum/models.py
+++ forum/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    user = models.ForeignKey(User, unique=True, related_name='posts')
+    user = models.ForeignKey(User, related_name='posts')
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
