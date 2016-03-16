c780297c60dc992d561acda2c6e9232dc01194cc
Vijayendra Bapte <vbapte@egnyte.com>
2016-03-16 12:11:38 -0700

    fixed posts on comments url; added few extra urls;

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,34 @@
 from django.conf.urls import url, include
 
 from rest_framework import routers
-from .views import UserViewSet, PostViewSet, CommentViewSet, MyPostsList
+from .views import (
+    UserViewSet, PostViewSet, CommentViewSet, MyPostsList,
+    OtherPostsList, MyCommentsList, OtherCommentsList,
+    CommentOnPostViewSet,
+    )
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r'', include(router.urls)),
+    url(r'^posts/(?P<post>[^/.]+)/comments/$',
+        CommentOnPostViewSet.as_view({'get': 'list',
+                                      'post': 'create',
+                                      }),
+        name='comments-on-posts-list'
+        ),
+    url(r'^posts/(?P<post>[^/.]+)/comments/(?P<pk>[^/.]+)/$',
+        CommentOnPostViewSet.as_view({'get': 'retrieve',
+                                      'put': 'update',
+                                      'delete': 'destroy',
+                                      }),
+        name='comments-on-posts-detail'),
+    url(r'^my/posts/$', MyPostsList.as_view(), name='my-posts-list'),
+    url(r'^my/comments/$', MyCommentsList.as_view(), name='my-posts-list'),
+    url(r'^other/posts/$', OtherPostsList.as_view(), name='other-posts-list'),
+    url(r'^other/comments/$', OtherCommentsList.as_view(), name='other-posts-list'),
+    ]
 
-urlpatterns.extend([
-    url(r'^myposts/$', MyPostsList.as_view(), name='my-posts-list'),
-    ])
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from rest_framework import viewsets, permissions, generics
 from rest_framework.decorators import detail_route, list_route
@@ -37,3 +37,40 @@ class MyPostsList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return user.posts.all()
+
+class OtherPostsList(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.exclude(user=user)
+
+class MyCommentsList(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrAuthenticated, )
+
+    def get_queryset(self):
+        user = self.request.user
+        return Comment.objects.filter(user=user)
+
+class OtherCommentsList(generics.ListAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, )
+
+    def get_queryset(self):
+        user = self.request.user
+        return Comment.objects.exclude(user=user)
+
+class CommentOnPostViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (IsOwnerOrAuthenticated, )
+
+    def perform_create(self, serializer):
+        post = generics.get_object_or_404(Post, pk=self.kwargs['post'])
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        return Comment.objects.filter(post__pk=self.kwargs["post"])
+
