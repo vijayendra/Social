54f14c0368fe4b63cf4d4a2928c1365f4b28d03d
Vijayendra Bapte <vbapte@egnyte.com>
2016-03-29 11:24:32 -0700

    heroku deploy setup

--- social/settings.py
+++ social/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,12 +78,16 @@ WSGI_APPLICATION = 'social.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-## DATABASES = {
-##     'default': {
-##         'ENGINE': 'django.db.backends.sqlite3',
-##         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-##     }
-## }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+db_from_env = dj_database_url.config(
+    conn_max_age=500,
+    )
+
 DATABASES['default'].update(db_from_env)
 
 
